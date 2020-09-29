bf0782851fb78b1bf8ddea0291d52c68b2f60f57
Jadeosull93 <jadeosullivan@hotmail.com>
2020-09-29 17:16:55 +0100

    Preparing to deploy to Heroku

--- ffl/settings.py
+++ ffl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,17 +84,20 @@ WSGI_APPLICATION = 'ffl.wsgi.application'
 # }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ffl_database', 
-        'USER': 'djangouser', 
-        'PASSWORD': 'djangouser90',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ffl_database', 
+#         'USER': 'djangouser', 
+#         'PASSWORD': 'djangouser90',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 
+DATABASES = {     'default':  dj_database_url.parse('postgres://bmhukkzcvbrudo:f0efd09b91a12d869dad3cd600e90906bf522e82621b6a4b7a3712c992d8a209@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dcr08rj67p5k70')
+
+  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

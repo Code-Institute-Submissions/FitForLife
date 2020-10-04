3711b29066af1acf78b421bee21101814a487e90
Jadeosull93 <jadeosullivan@hotmail.com>
2020-10-04 14:53:47 +0000

    Debugging Heroku issues

--- ffl/settings.py
+++ ffl/settings.py
@@ -138,7 +138,16 @@ else:
         }
     }
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ffl_database', 
+        'USER': 'djangouser', 
+        'PASSWORD': 'djangouser90',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

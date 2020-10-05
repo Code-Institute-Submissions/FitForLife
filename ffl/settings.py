dd98693d7e1e26154061213fd1e57b80f6c27401
Jadeosull93 <jadeosullivan@hotmail.com>
2020-10-05 22:38:20 +0100

    Fixing issues with Heroku

--- ffl/settings.py
+++ ffl/settings.py
@@ -146,7 +146,7 @@ else:
         }
     }
 if heroku:
-    DATABASES = { 'default': dj_database_url.parse('postgres://bmhukkzcvbrudo:f0efd09b91a12d869dad3cd600e90906bf522e82621b6a4b7a3712c992d8a209@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dcr08rj67p5k7') }
+    DATABASES = { 'default': dj_database_url.parse('postgres://bmhukkzcvbrudo:f0efd09b91a12d869dad3cd600e90906bf522e82621b6a4b7a3712c992d8a209@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dcr08rj67p5k70') }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

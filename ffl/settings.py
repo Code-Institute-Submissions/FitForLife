7a7f61acb2eabc1dbe3e2fda4f1caf8823cef211
Jadeosull93 <jadeosullivan@hotmail.com>
2020-10-07 19:58:21 +0100

    Changed base directory

--- ffl/settings.py
+++ ffl/settings.py
@@ -54,7 +54,9 @@ heroku_postgres = os.environ.get('HEROKU_POSTGRES',"True") # Use a Heroku hosted
 DEBUG=development
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production

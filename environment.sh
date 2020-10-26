a8baae600d2ea6389b281946c3450c4cff9604ce
Jadeosull93 <jadeosullivan@hotmail.com>
2020-10-26 18:52:30 +0000

    Changed superuser

--- environment.sh
+++ environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DEVELOPMENT="True"
 export HEROKU="False"
-export SECRET_KEY="MySecretKey"
-export HEROKU_POSTGRES="False"
-#export DATABASE_URL="postgres://bmhukkzcvbrudo:f0efd09b91a12d869dad3cd600e90906bf522e82621b6a4b7a3712c992d8a209@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dcr08rj67p5k70"
+export SECRET_KEY="secretkeyfordjango"
+export HEROKU_POSTGRES="True"
+export HEROKU_POSTGRES="True"
+export DATABASE_URL="postgres://mtajlhnjsvfhxr:56013e42e5b515c8c488fd890f4aa2351f4f47e6b39ed926703339ead2d6fd64@ec2-54-170-123-247.eu-west-1.compute.amazonaws.com:5432/db6esjaiarmhtb"
 
- 

c350a250f6c19061c5ad66f1aa9053f8470d3bfa
Jadeosull93 <jadeosullivan@hotmail.com>
2020-11-26 19:19:20 +0000

    Work in progress

--- plans/webhooks.py
+++ plans/webhooks.py
@@ -4,12 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 @csrf_exempt  
-@require_POST
+#@require_POST
 def webhook(request):
     jsondata = request.body
     # data = json.loads(jsondata)
     # for answer in data['form_response']['answers']: # go through all the answers
     #   type = answer['type']
-    print("webhook called") # print value of answers
+    print("webhook called:jsondata " + str(jsondata) ) # print value of answers
 
     return HttpResponse(status=200)

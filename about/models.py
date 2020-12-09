a24bbf4f4c7d214fa846c34eb9dd33cb376f7e23
Jadeosull93 <jadeosullivan@hotmail.com>
2020-12-09 08:19:11 +0000

    changed_contact_ids

--- about/models.py
+++ about/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
+#option to store contact messages
 #class About(models.Model):
     # contact_email = models.EmailField(max_length=254,null=False)
     # contact_name = models.TextField(max_length=30,null=False)
